410cd21b3a8da5363b34bac698dae1b0b859d0ff
SyntaxisTaxsyn <66139375+SyntaxisTaxsyn@users.noreply.github.com>
2025-07-19 18:17:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/DartsScorer';
 var APP_PREFIX = 'DartsScorer_';
-var VERSION = 'version_002';
+var VERSION = 'version_003';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
